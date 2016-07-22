39cac82312dffdc8233225ee74b27eeb35e8ea88
priyavpd <vpriyacse2@gmail.com>
2016-07-22 10:03:50 +0530

    Create Positivenum.java

---
+++ Positivenum.java
@@ -0,0 +1,16 @@
+# Program
+public class Positivenum
+{
+public static void main(String[] args)
+{
+int a=10;
+if(a>0)
+{
+System.out.println("positive number");
+}
+else
+{
+System.out.println("negative  number");
+}
+}
+}
